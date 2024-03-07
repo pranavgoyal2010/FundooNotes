77366e3c32acc3607794145381eee1c6a9d275a6
Pranav Goyal <pr945005@dal.ca>
2024-03-07 15:56:59 +0530

    [Pranav] Add : getting started

---
+++ FundooAPI/RepositoryLayer/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace RepositoryLayer.Context;
+
+public class AppDbContext
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("SqlConnection");
+    }
+    public IDbConnection CreateConnection()
+    {
+        return new SqlConnection(_connectionString);
+    }
+}
