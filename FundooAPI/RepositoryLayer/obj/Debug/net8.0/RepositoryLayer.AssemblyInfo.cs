7826ca9c640760b7d7e4ed73701f60ef9f425d4e
Pranav Goyal <pr945005@dal.ca>
2024-03-19 18:39:38 +0530

    [Pranav] Added: HTML code added to reset password email

--- FundooAPI/RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
+++ FundooAPI/RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+522284f35bc20762802e5065a58f5089cd81a1b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a396b0e0ccea5229e149fcde2690c49b400fcf58")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
